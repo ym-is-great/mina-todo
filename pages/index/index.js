84d873dc38de6989e5d40c137d7eb83e4a7e2989
cym <hi@caiyiming.com>
2017-06-07 01:31:48 +0800

    更新

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,62 +1,44 @@
 // pages/index/index.js
 Page({
-  data:{
-    input: {
-      class: '',
-      value: ''
+    data: {
+        newItem: '',
+        items: []
     },
-    items:[
-    ]
-  },
-  /**
-   * 输入框获取焦点事件
-   */
-  focus: function(e) {
-    this.setData({ 'input.class': 'focused' });
-  },
-  /**
-   * 输入框失去焦点事件
-   */
-  blur: function(e) {
-    this.setData({ 'input.class': '' });
-  },
-  /**
-   * 输入框内容提交事件
-   */
-  confirm: function(e) {
-    if (e.detail.value) {
-      this.data.items.push({ name: e.detail.value, status: 0 });
-      this.setData({ 'items' : this.data.items });
-      this.setData({ 'input.value' : '' });
+    // 输入项目
+    inputItem: function (event) {
+        this.data.newItem = event.detail.value
+    },
+    // 添加事项
+    addItem: function () {
+        if (this.data.newItem) {
+            this.data.items.push({ content: this.data.newItem, status: 0 });
+            this.setData({ 
+                'items': this.data.items,
+                'newItem': ''
+            });
+        }
+    },
+    // 选中待办事项
+    check: function (e) {
+        let index = e.detail.value[0];
+        this.data.items[index].status = 1;
+        this.setData({ 'items': this.data.items });
+    },
+    // 取消选中待办事项
+    uncheck: function (e) {
+        for (let i = 0; i < this.data.items.length; i++) {
+            this.data.items[i].status = 0;
+        }
+        for (let i = 0; i < e.detail.value.length; i++) {
+            let index = e.detail.value[i];
+            this.data.items[index].status = 1;
+        }
+        this.setData({ 'items': this.data.items });
+    },
+    // 删除待办事项
+    removeItem: function (e) {
+        let index = e.target.dataset.index;
+        this.data.items.splice(index, 1);
+        this.setData({ 'items': this.data.items });
     }
-  },
-  /**
-   * 复选框选中事件
-   */
-  check: function(e) {
-    let index = e.detail.value[0];
-    this.data.items[index].status = 1;
-    this.setData({ 'items' : this.data.items });
-  },
-  /**
-   * 复选框取消选中事件
-   */
-  uncheck: function(e) {
-    for (let i=0; i<this.data.items.length; i++) {
-      this.data.items[i].status = 0;
-    }
-    for (let i=0; i<e.detail.value.length; i++) {
-      let index = e.detail.value[i];
-      this.data.items[index].status = 1;
-    }
-    this.setData({ 'items' : this.data.items });
-  },
-  /**
-   * 删除按钮点击事件
-   */
-  remove: function(e) {
-    let index = e.target.dataset.index;
-    this.data.items.splice(index, 1);
-    this.setData({ 'items' : this.data.items });
-  }
 })
