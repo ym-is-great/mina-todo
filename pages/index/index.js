f74ea172f0320a4f0931a6b91934b4eac238fe87
cym <cym@wunsun.com>
2017-06-07 14:09:48 +0800

    开发

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,41 +4,53 @@ Page({
         newItem: '',
         items: []
     },
-    // 输入项目
+    onLoad: function () {
+        let items = wx.getStorageSync('items')
+        if (items) {
+            this.setData({
+                'items': items
+            })
+        }
+    },
+    onUnload: function () {
+    },
+    // 缓存事项
+    saveItems: function () {
+        if (this.data.items.length) {
+            wx.setStorageSync('items', this.data.items)
+        }
+    },
+    // 输入事项
     inputItem: function (event) {
         this.data.newItem = event.detail.value
     },
     // 添加事项
     addItem: function () {
         if (this.data.newItem) {
-            this.data.items.push({ content: this.data.newItem, status: 0 });
+            this.data.items.push({ content: this.data.newItem, completed: false })
             this.setData({ 
                 'items': this.data.items,
                 'newItem': ''
-            });
+            })
+            this.saveItems()
         }
     },
-    // 选中待办事项
-    check: function (e) {
-        let index = e.detail.value[0];
-        this.data.items[index].status = 1;
-        this.setData({ 'items': this.data.items });
-    },
-    // 取消选中待办事项
-    uncheck: function (e) {
-        for (let i = 0; i < this.data.items.length; i++) {
-            this.data.items[i].status = 0;
-        }
-        for (let i = 0; i < e.detail.value.length; i++) {
-            let index = e.detail.value[i];
-            this.data.items[index].status = 1;
-        }
-        this.setData({ 'items': this.data.items });
+    // 勾选事项
+    checkItem: function (event) {
+        let index = event.currentTarget.dataset.index
+        this.data.items[index].completed = !this.data.items[index].completed
+        this.setData({
+            'items': this.data.items
+        })
+        this.saveItems()
     },
     // 删除待办事项
-    removeItem: function (e) {
-        let index = e.target.dataset.index;
-        this.data.items.splice(index, 1);
-        this.setData({ 'items': this.data.items });
+    removeItem: function (event) {
+        let index = event.currentTarget.dataset.index
+        this.data.items.splice(index, 1)
+        this.setData({
+            'items': this.data.items
+        })
+        this.saveItems()
     }
 })
